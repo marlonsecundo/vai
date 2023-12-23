8b6740c526bb1f9a41b3b94497a0ef56c04bc013
Marlon Secundo <marlon_secundo@outlook.com>
2023-12-23 14:53:14 -0300

    wip

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const withNextIntl = require("next-intl/plugin")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/vai",
-  assetPrefix: "/vai/",
   output: debug ? "standalone" : "export",
 };
 
