66899616f75e0873b4608781d7005445fbee3d8f
Marlon Secundo <marlon_secundo@outlook.com>
2023-12-23 15:02:04 -0300

    wip

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+  return NextResponse.json({ hee: "asdsa" });
 }
 
 // See "Matching Paths" below to learn more
