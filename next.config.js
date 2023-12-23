a403a8f17af6783830560122dc84cd2a096755bc
Marlon Secundo <marlon_secundo@outlook.com>
2023-12-23 14:30:23 -0300

    wip

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const debug = process.env.NODE_ENV !== "production";
+const withNextIntl = require("next-intl/plugin")();
 
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/vai",
+  assetPrefix: "/vai/",
+  output: debug ? "standalone" : "export",
 };
+
+module.exports = nextConfig;
